c7305aca5025aaff912c53392b44e23849a09c1c
codenameryuu <leaderballon@gmail.com>
2026-08-10 15:14:03 +0700

    Refactor test suite and update Cypress dependencies: Improved test organization by removing outdated test files, upgraded Cypress to version 15.20.0, and updated related dependencies in yarn.lock for better compatibility and performance.

---
+++ cypress/e2e/overview/bin/delete.cy.js
@@ -0,0 +1,47 @@
+import { faker } from "@faker-js/faker";
+
+describe("Delete Data Spec", () => {
+  beforeEach(() => {
+    cy.login();
+  });
+
+  it("Should delete data successfully with valid data", () => {
+    // * Intercept the get list bin API
+    cy.intercept("GET", "**/api/v1/dashboard/bin?**").as("getListBin");
+
+    // * Click on the all drafts sidebar item
+    cy.contains("a[href='/dashboard/category/bin']", "Bin").should("be.visible").click();
+    cy.wait(1000);
+
+    // * Wait for the get list bin API to be called
+    cy.wait("@getListBin")
+      .its("response")
+      .should((response) => {
+        expect(response.statusCode).to.eq(200);
+        expect(response.body).to.deep.include({ status: true });
+        expect(response.body.data.data).to.be.an("array").and.not.be.empty;
+      });
+
+    // * Click first data on the table
+    cy.get(".ag-center-cols-container .ag-row").should("have.length.at.least", 1).first().click();
+    cy.wait(1000);
+
+    // * Click on the delete button
+    cy.contains("button.button-bin", "Delete").scrollIntoView().should("be.visible").click();
+    cy.wait(1000);
+
+    // * Intercept the delete bin API
+    cy.intercept("DELETE", "**/api/v1/dashboard/delete-bin").as("deleteBin");
+
+    // * Click on the confirm button in modal
+    cy.get("#updatetConfirmModal").should("be.visible").contains("button.btn-danger", "Delete").should("be.visible").click();
+
+    // * Wait for the delete bin API to be called
+    cy.wait("@deleteBin")
+      .its("response")
+      .should((response) => {
+        expect(response.statusCode).to.eq(200);
+        expect(response.body).to.deep.include({ status: true });
+      });
+  });
+});
---
+++ cypress/e2e/overview/bin/restore.cy.js
@@ -0,0 +1,47 @@
+import { faker } from "@faker-js/faker";
+
+describe("Restore Data Spec", () => {
+  beforeEach(() => {
+    cy.login();
+  });
+
+  it("Should restore data successfully with valid data", () => {
+    // * Intercept the get list bin API
+    cy.intercept("GET", "**/api/v1/dashboard/bin?**").as("getListBin");
+
+    // * Click on the all drafts sidebar item
+    cy.contains("a[href='/dashboard/category/bin']", "Bin").should("be.visible").click();
+    cy.wait(1000);
+
+    // * Wait for the get list bin API to be called
+    cy.wait("@getListBin")
+      .its("response")
+      .should((response) => {
+        expect(response.statusCode).to.eq(200);
+        expect(response.body).to.deep.include({ status: true });
+        expect(response.body.data.data).to.be.an("array").and.not.be.empty;
+      });
+
+    // * Click first data on the table
+    cy.get(".ag-center-cols-container .ag-row").should("have.length.at.least", 1).first().click();
+    cy.wait(1000);
+
+    // * Click on the restore button
+    cy.contains("button.button-bin", "Restore").scrollIntoView().should("be.visible").click();
+    cy.wait(1000);
+
+    // * Intercept the restore bin API
+    cy.intercept("DELETE", "**/api/v1/dashboard/restore-bin").as("restoreBin");
+
+    // * Click on the confirm button in modal
+    cy.get("#updatetConfirmModal").should("be.visible").contains("button.upload-button-next", "Restore").should("be.visible").click();
+
+    // * Wait for the restore bin API to be called
+    cy.wait("@restoreBin")
+      .its("response")
+      .should((response) => {
+        expect(response.statusCode).to.eq(200);
+        expect(response.body).to.deep.include({ status: true });
+      });
+  });
+});
